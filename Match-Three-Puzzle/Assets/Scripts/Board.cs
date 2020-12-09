344f1b6b8b8da5c5950868ab7cca65cfe3f7e548
htaskin21 <hasantaskin21@outlook.com>
2020-12-10 00:30:42 +0300

    initial commit

---
+++ Match-Three-Puzzle/Assets/Scripts/Board.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public GameObject[] gamePiecePrefabs;
+    public int height;
+    public int width;
+
+    public int borderSize;
+
+    Tile[,] _allTiles;
+    GamePiece[,] _allGamePieces;
+
+    void Start()
+    {
+        _allTiles = new Tile[width, height];
+        _allGamePieces = new GamePiece[width, height];
+        SetupTiles();
+        SetupCamera();
+        FillRandom();
+    }
+
+    void SetupTiles()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject tile = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+                tile.name = $"Tile({i},{j})";
+                _allTiles[i, j] = tile.GetComponent<Tile>();
+                _allTiles[i, j].Init(i, j, this);
+                tile.transform.parent = transform;
+            }
+        }
+    }
+
+    void SetupCamera()
+    {
+        Camera.main.transform.position = new Vector3((((float)width - 1f) / 2f), (((float)height - 1f) / 2f), -10f);
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+        float vertical = (float)height / 2 + borderSize;
+        float horizontal = ((float)width / 2 + borderSize) / aspectRatio;
+        Camera.main.orthographicSize = (horizontal > vertical) ? horizontal : vertical;
+    }
+
+    GameObject GetRandomGamePiece()
+    {
+        int randomIndex = Random.Range(0, gamePiecePrefabs.Length);
+        if (gamePiecePrefabs[randomIndex] == null)
+        {
+            Debug.LogWarning("Board: " + randomIndex + "does not contain valid prefab");
+        }
+
+        return gamePiecePrefabs[randomIndex];
+    }
+
+    void PlaceGamePiece(GamePiece gamePiece, int x, int y)
+    {
+        if (gamePiece == null)
+        {
+            Debug.LogWarning("Board: Invalid GamePiece");
+            return;
+        }
+        gamePiece.transform.position = new Vector3(x, y, 0);
+        gamePiece.transform.rotation = Quaternion.identity;
+        gamePiece.SetCoord(x, y);
+    }
+
+    void FillRandom()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject randomPiece = Instantiate(GetRandomGamePiece(), Vector3.zero, Quaternion.identity) as GameObject;
+                if (randomPiece != null)
+                {
+                    PlaceGamePiece(randomPiece.GetComponent<GamePiece>(), i, j);
+                }
+            }
+        }
+    }
+}
---
+++ Match-Three-Puzzle/Assets/Scripts/GamePiece.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePiece : MonoBehaviour
+{
+    public int xIndex;
+    public int yIndex;
+
+    bool _isMoving = false;
+
+    public void SetCoord(int x, int y)
+    {
+        xIndex = x;
+        yIndex = y;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move((int)transform.position.x + 1, (int)transform.position.y, 0.5f);    
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move((int)transform.position.x - 1, (int)transform.position.y, 0.5f);
+        }
+    }
+
+    void Move(int destX, int destY, float timeToMove)
+    {
+        StartCoroutine(MoveRoutine(new Vector3(destX,destY,0), timeToMove));
+    }
+
+    IEnumerator MoveRoutine(Vector3 destination, float timeToMove)
+    {
+        Vector3 startPosition = transform.position;
+
+        bool reachedDestination = false;
+        float elapsadTime = 0f;
+        _isMoving = true;
+
+        while (!reachedDestination)
+        {
+            if (Vector3.Distance(transform.position, destination) < 0.01f)
+            {
+                reachedDestination = true;
+                transform.position = destination;
+                SetCoord((int)destination.x, (int)destination.y);
+                break;
+            }
+            elapsadTime += Time.deltaTime;
+            float t = Mathf.Clamp(elapsadTime / timeToMove, 0f, 1f);
+            transform.position = Vector3.Lerp(startPosition, destination, t);
+            yield return null;
+        }
+        _isMoving = false;
+    }
+}
